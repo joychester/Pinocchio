cf03aed564c73acaf67dd54743bfd91d27d78b55
Cheng Chi (@joychester) <joychester@gmail.com>
2019-04-29 22:34:06 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const pup = require('puppeteer');
+const myTest = require('./projects/mytest');
+
+(async() => {
+    // return: a Promise, which resolves to browser instance.
+    const browser = await pup.launch({
+      // Set of configurable options to set on the browser.
+      headless: false,
+      devtools: true,
+      //slowMo: 100,
+      //args: ['--enable-feature=NetworkService'],
+      //ignoreHTTPSErrors: true,
+    });
+
+    //a Promise, The Page is created in a default browser context.
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.setRequestInterception(false);
+
+    try {
+      console.log(await myTest(page));
+    }
+    catch(err) {
+      console.log("Error Message: " + err.stack);
+    }
+    finally {
+      await browser.close();
+    }
+})();
