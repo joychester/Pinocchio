a37375025dc5be3c3e15b8483982691f93b6d214
Cheng Chi (@joychester) <joychester@gmail.com>
2019-05-09 23:12:22 -0700

    Create global_config.js

---
+++ global_config.js
@@ -0,0 +1,14 @@
+const GlConfigs = {
+  Thread_sleep: 250,
+
+  ES_connection_info: 'http://localhost:9200',
+  ES_client_max_retry: 3,
+  ES_client_requestTimeout: 20000,
+  ES_sniffOnStart: 'true',
+
+  Pup_Headless: 'false',
+  Pup_devtools: 'true'
+
+}
+
+module.exports = GlConfigs;
